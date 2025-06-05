a313cd8a86083118e77009b8146f3184519a59f8
Muhammad pen <muhammadpen@gmail.com>
2025-06-05 17:03:20 +0500

    astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://muhammadpen.github.io/',
+    base: '/muhammadpen.github.io'
+});
