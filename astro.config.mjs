3459151fc5ae6c596248d3d4b33343ee37a94756
Muhammad pen <muhammadpen@gmail.com>
2025-06-05 19:00:18 +0500

    hi hi hi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://muhammadpen.github.io/',
-    base: '/muhammadpen.github.io'
+  site: 'https://muhammadpen.github.io/',
+  base: '/muhammadpen.github.io',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
